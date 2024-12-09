a51b0bc681a10acbe96c71473f5f3ecd492c5aad
davyken <davykennang552@gmail.com>
2024-12-09 11:21:23 +0100

    modified content

--- client/src/pages/landingPage/LandingPage.jsx
+++ client/src/pages/landingPage/LandingPage.jsx
@@ -22,7 +22,7 @@ const LandingPage = () => {
             </ul>
           </nav>
           <button className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700">
-          <Link to="/signup" className="px-6 py-3 bg-blue-500 text-white rounded hover:bg-blue-700">
+          <Link to="/signup" className="px-6 py-3  text-white rounded hover:bg-blue-700">
           Sign Up
           </Link>
           </button>
